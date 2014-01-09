77a59933098b1a68640c7ddc9ad6a27475aa0a2b
dipak <11dipak.contact@gmail.com>
2014-01-09 18:59:07 +0530

    Update Portfolio

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,18 +3,11 @@ class Project < ActiveRecord::Base
 	#extend FriendlyId
   #friendly_id :slug, use: :history
 	
-	  #has_attached_file :avatar, 
-    #:styles => { :pfolio_e => "650x600!",:pfolio => "310x230!" },
-    #:url => "/project/:id/:style/:basename.:extension",
-    #:path => ":rails_root/public/project/:id/:style/:basename.:extension"
+	has_attached_file :avatar, 
+    :styles => { :pfolio_e => "650x600!",:pfolio => "370x289!" },
+    :url => "/project/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/project/:id/:style/:basename.:extension"
 
-
-	has_attached_file :avatar,
-     :styles => { :thumb=> "370x289#{}"},
-     :storage => :s3,
-     :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
-     :path => "/project/:id/:style/:filename"
-  
 	validates :project_title, :short_description, :project_type, :long_description, :presence => true
 
 end
