d14506a811b71505481b59222927776610f2d832
dipak <11dipak.contact@gmail.com>
2014-01-20 20:40:00 +0530

    fixing issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,18 @@
 KeshariyaCompany::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Do not eager load code on boot.
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both thread web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
