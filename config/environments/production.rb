1e4fc6f590ff8a7e2770b14527d176484a3aab05
dipak <11dipak.contact@gmail.com>
2014-01-20 20:43:56 +0530

    fixing issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,20 @@ KeshariyaCompany::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Don't care if the mailer can't send.
+  # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # config.action_dispatch.rack_cache = true
+
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
