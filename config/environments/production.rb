89794f97ff254b688de85bc7dc182eb409b940e1
dipak <11dipak.contact@gmail.com>
2014-04-03 01:18:09 +0530

    fixing issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ KeshariyaCompany::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( active_admin.css active_admin/print.css active_admin.js application.css application.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
